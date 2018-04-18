d0f90e8e9d942d934817cc2c064aad85c7eab829
rhe24 <rhe24@aber.ac.uk>
2018-04-18 14:57:31 +0100

    Created Skeleton for main file

--- src/maze_runner/main.c
+++ src/maze_runner/main.c
@@ -1,65 +1,32 @@
-/* Simple skeletontest program for in-robot API */
+/**
+  * Main file of the 'Maze Runner' hybrid robot controller
+  * @author Rhys Evans (rhe24@aber.ac.uk)
+  * @version 0.1
+*/
 
 #include "allcode_api.h"
 
-   
-int main()
-{
-	FA_RobotInit();
-    
-    FA_LCDBacklight(50);
 
-    while(1)
-    {   
-        /*
+/**
+  * Initialize the application by starting various parts of the state machine
+  * and loading the robot's API
+*/
+int init(){
+  FA_RobotInit();
 
-        // Uncomment this code to activate motor functions
-
-        // Go forward
-        FA_SetMotors(20, 20);
-      
-        if(FA_ReadIR(1) > 600)  // check front left ir
-        {
-            FA_Backwards(50);
-            FA_Right(30);      
-        }
-
-        if(FA_ReadIR(2) > 600)  // check front ir
-        {
-            FA_Backwards(50);
-        }
-
-        if(FA_ReadIR(3) > 600)  // check front right ir
-        {
-            FA_Backwards(50);
-            FA_Left(30);      
-        }
-
-        */
-
-	FA_LCDClear();
-        FA_LCDPrint("Henlo", 5, 20,25,FONT_NORMAL, LCD_OPAQUE);
-
-        
-        if(FA_ReadSwitch(0) == 1)
-            FA_PlayNote(1200,200);
-        if(FA_ReadSwitch(1) == 1)
-            FA_PlayNote(2200,200);
-        
-		FA_DelayMillis(100);
-        
-        int i;
-        for (i = 0; i < 8; i++) {
-            if (FA_ReadIR(i) > 600) {
-                FA_LEDOn(i);
-            }
-            else {
-                FA_LEDOff(i);
-            }
-        }
-    }            
- 	return 0;
+  FA_LCDBacklight(50);
 }
 
 
+/**
+  *
+*/
+int main(){
+  init();
 
+  // Main Loop
+  while(1){
+
+  }
+
+}
